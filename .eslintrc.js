6ecb1d8267c982d2be7c4297879f8a028946eec3
kmsheng <kmsh3ng@gmail.com>
2017-08-27 22:48:44 +0800

    ignore object-curly-spacing and space-before-function-paren

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,8 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    "comma-dangle": 0
+    "comma-dangle": 0,
+    "object-curly-spacing": 0,
+    "space-before-function-paren": 0
   }
 }
