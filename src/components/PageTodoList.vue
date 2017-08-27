9d5dfd12afc6b37483f5363aa597411d06c70145
kmsheng <kmsh3ng@gmail.com>
2017-08-27 23:09:11 +0800

    add example for view toggling

--- src/components/PageTodoList.vue
+++ src/components/PageTodoList.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="page-todo-list">
     <h1>Todo List</h1>
+    <button v-on:click="toggleTodoList()">toggle todo list</button>
     <form v-on:submit.prevent="addTodo">
       <input type="text" v-model="todoMessage" />
     </form>
-    <ul>
+    <ul v-show="isTodoListVisible">
       <li v-for="todo in todos">{{todo.message}}</li>
     </ul>
   </div>
@@ -15,6 +16,7 @@ export default {
   name: 'PageTodoList',
   data() {
     return {
+      isTodoListVisible: true,
       todoMessage: '',
       todos: [
         {message: 'Foo'},
@@ -26,6 +28,9 @@ export default {
     addTodo() {
       this.todos.push({message: this.todoMessage});
       this.todoMessage = '';
+    },
+    toggleTodoList() {
+      this.isTodoListVisible = ! this.isTodoListVisible;
     }
   }
 };
