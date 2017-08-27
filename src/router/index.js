925dcabd875bffc9a835c2d0694e561aa45af8dc
kmsheng <kmsh3ng@gmail.com>
2017-08-27 22:49:46 +0800

    define route for PageTodoList

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import PageTodoList from '@/components/PageTodoList';
 
 Vue.use(Router);
 
@@ -11,5 +12,10 @@ export default new Router({
       name: 'Hello',
       component: Hello,
     },
+    {
+      path: '/todo-list',
+      name: 'PageTodoList',
+      component: PageTodoList
+    }
   ],
 });
