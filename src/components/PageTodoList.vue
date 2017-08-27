b5bd8f1bd0ad513e3723e40c1fad7b7168f7e146
kmsheng <kmsh3ng@gmail.com>
2017-08-27 22:49:14 +0800

    add component PageTodoList

---
+++ src/components/PageTodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page-todo-list">
+    <h1>Todo List</h1>
+    <form v-on:submit.prevent="addTodo">
+      <input type="text" v-model="todoMessage" />
+    </form>
+    <ul>
+      <li v-for="todo in todos">{{todo.message}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTodoList',
+  data() {
+    return {
+      todoMessage: '',
+      todos: [
+        {message: 'Foo'},
+        {message: 'Bar'}
+      ]
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({message: this.todoMessage});
+      this.todoMessage = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
