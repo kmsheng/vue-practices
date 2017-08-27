34ca5e421e265a34902faa723a7239e995585212
kmsheng <kmsh3ng@gmail.com>
2017-08-27 23:08:47 +0800

    ignore space-unary-ops

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     "comma-dangle": 0,
     "object-curly-spacing": 0,
-    "space-before-function-paren": 0
+    "space-before-function-paren": 0,
+    "space-unary-ops": 0
   }
 }
