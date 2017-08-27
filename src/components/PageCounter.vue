88ebba31e5d65fff5a8a51ccdf4683e92fd9899f
kmsheng <kmsh3ng@gmail.com>
2017-08-28 00:50:43 +0800

    add component PageCounter

---
+++ src/components/PageCounter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-counter">
+    <h1>Page Counter</h1>
+    <button v-on:click="add()">add</button>
+    <button v-on:click="minus()">minus</button>
+    <div>sign: {{sign}}</div>
+    <div>{{$store.state.count}}</div>
+  </div>
+</template>
+
+<script>
+import {mapActions, mapGetters} from 'vuex';
+
+export default {
+  name: 'PageCounter',
+  computed: mapGetters([
+    'sign'
+  ]),
+  methods: mapActions([
+    'add',
+    'minus'
+  ])
+};
+</script>
+
+<style scoped>
+</style>
