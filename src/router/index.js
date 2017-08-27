c96562faeb5595a2838d5009631aa3c2c40d59a1
kmsheng <kmsh3ng@gmail.com>
2017-08-28 00:51:13 +0800

    add new route PageCounter

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import PageTodoList from '@/components/PageTodoList';
+import PageCounter from '@/components/PageCounter';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/todo-list',
       name: 'PageTodoList',
       component: PageTodoList
+    },
+    {
+      path: '/counter',
+      name: 'PageCounter',
+      component: PageCounter
     }
   ],
 });
